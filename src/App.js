7d6b7901ef0503ae27dd61316c9c8cc0e9258e8e
Jack Anderson <whotfisjax@gmail.com>
2024-04-28 12:34:36 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import buffVideo from './buffonsol.mp4'; // Import the buff video
+import buffImage from './buff2.png'; // Import the buff imag
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen w-screen flex justify-center items-center text-6xl">
+      <video autoPlay loop muted className="absolute inset-0 object-cover w-full h-full z-[-1]">
+        <source src={buffVideo} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className='grid text-center'>
+        Get $buff
+        <div className='flex justify-center p-8'><img className="w-[40%]" src={buffImage} alt="Buff" /></div>
+        <div className='flex justify-center'>
+          <a href="" className='bg-[#f3f3f3] w-min p-4'>
+            Buy
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
